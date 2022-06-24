1f1ceba920fdf10569ca967c16b3c61c8679c974
Elijah Lennon <elijahlennon1@gmail.com>
2022-06-24 17:53:18 -0500

    completed exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public WordSmith()
+        {
+        }
+        public bool IsAPalindrome(string word)
+        {
+            var reversed = "";
+
+                for (var i = word.Length - 1; i >= 0; i--)
+            {
+                reversed += word.ToLower()[i];
+            }
+            if (reversed == word.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,29 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("kayak", true)]
+        [InlineData("madam", true)]
+        [InlineData("star rats", true)]
+        [InlineData("elijah", false)]
+        [InlineData("nurses run", true)]
+        public void PalindromeTester(string word, bool expected)
         {
+            //Arrange
+            var tester = new WordSmith();
 
+            //act
+            var actual = tester.IsAPalindrome(word);
+
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
